45b75926e184ec4c065a71b06d42d840e21e6a2e
SuzukiYuya <mcd2376083@stu.o-hara.ac.jp>
2025-09-11 11:04:35 +0900

    ゲームの設定を管理するクラスを作成

---
+++ Roguehand/Assets/takumi/GameConfig.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲームの設定を管理するクラス
+/// </summary>
+public class GameConfig
+{
+    /// <summary>
+    /// ゲームの速度を管理する変数
+    /// </summary>
+    int _gameSpeed = 1;
+
+
+}
