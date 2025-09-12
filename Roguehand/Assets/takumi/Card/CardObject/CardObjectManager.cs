c14891935065fafca1d9d891e9dc57f6b503d6a8
Salre <mcd2376019@stu.o-hara.ac.jp>
2025-09-12 09:52:49 +0900

    カードの元のオブジェクトを追加

---
+++ Roguehand/Assets/takumi/Card/CardObject/CardObjectManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardObjectManager : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBase; 
+}
