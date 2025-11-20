d3fed06148f829daccf0bae00a4ff77267710f1f
SuzukiYuya <mcd2376083@stu.o-hara.ac.jp>
2025-11-20 10:34:50 +0900

    パック購入時並び替え発生

--- Roguehand/Assets/Script/Suzuki/Shop/TesPack.cs
+++ Roguehand/Assets/Script/Suzuki/Shop/TesPack.cs
@@ -23,12 +23,16 @@ public class TesPack : MonoBehaviour
     {
         none = -1,
         joker,
-        card,
+        item,
+        spectrum,
+        trump,
         max
     }
 
     private void Update()
     {
+        Debug.Log(_packs.Count);
+
         if (!ShopManager.instance.IsShop()) return;
         PackCreate();
 
@@ -59,9 +63,9 @@ public class TesPack : MonoBehaviour
 
                     Debug.Log("パックを購入したよー");
                     // パックの購入時の処理を描く
-
                     GameObject domyy = _packs[cash];
                     SaleObjectManager.instance.Remove(domyy);
+                    BuyTrans(cash);
 
 
                 }
@@ -81,13 +85,42 @@ public class TesPack : MonoBehaviour
     {
         // leftとrightから直線を作り、線を分割することで中心点を出す
         // 終点に乗らないように+1する(後ろを増やす)
-        int num = _packs.Count + 1;
+        int num=0;
+        for(int i=0; i < _packs.Count; i++)
+        {
+            if (_packs[i]==null)continue;
+            num++;
+        }
+        num += 1;
+        int minus = 0;
         for (int i = 0; i < _packs.Count; i++)
         {
+            if (_packs[i] == null)
+            {
+                minus++;
+                continue; 
+            }
             // 始点に乗らないように+1する(前を増やす)
-            float dis = (float)(i + 1) / num;
+            // 購入済みパックは考慮させないようminusをはさむ
+            float dis = (float)(i- minus + 1) / num;
             _packs[i].transform.position = Vector3.Lerp(_leftTargetPos.position, _rightTargetPos.position, dis);
         }
+    }
+
+    /// <summary>
+    /// パック購入時並び替えをする
+    /// </summary>
+    /// <param name="ID">購入されたパックのID</param>
+    private void BuyTrans(int ID)
+    {
+        _packs[ID]=null;
+        Trans();
+
+    }
+
+    public List<GameObject> GetList()
+    {
+        return _packs;
     }
 
 }
