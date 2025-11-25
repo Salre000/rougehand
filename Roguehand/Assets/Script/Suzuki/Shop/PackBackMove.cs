36525e65bc1c804dc55bbb62ff98033689ccc13b
SuzukiYuya <mcd2376083@stu.o-hara.ac.jp>
2025-11-25 14:26:37 +0900

    パック開封時に専用背景がでるようにした

---
+++ Roguehand/Assets/Script/Suzuki/Shop/PackBackMove.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// パック購入時専用背景が移動、パック終了時にも移動
+/// </summary>
+public class PackBackMove : MonoBehaviour
+{
+    [SerializeField] private GameObject _packBack;
+    [SerializeField] private Transform _targetPos;
+    private Vector3 _originPos=new();
+    private float _time = 0.05f;
+
+    private void Awake()
+    {
+        _originPos=Vector3.zero;
+        _packBack.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        BuyPackBackMove();
+    }
+
+    /// <summary>
+    /// 購入時背景が来る
+    /// </summary>
+    private void BuyPackBackMove()
+    {
+        if(!PackManager.instance.IsBuyPack()) return;
+        if ((_packBack.transform.localPosition - _originPos).sqrMagnitude < 0.001f) return;
+        _packBack.SetActive(true);
+        _packBack.transform.localPosition = Vector3.Lerp(_packBack.transform.localPosition, _originPos, _time);
+        
+    }
+    /// <summary>
+    /// パック終了時背景が帰る
+    /// </summary>
+    private void EndPack()
+    {
+        if (PackManager.instance.IsBuyPack()) return;
+        if (!_packBack.activeSelf) return;
+        if ((_packBack.transform.localPosition - _targetPos.localPosition).sqrMagnitude < 0.001f)
+        {
+            _packBack.SetActive(false);
+            return;
+        }
+        _packBack.transform.localPosition = Vector3.Lerp(_packBack.transform.localPosition, _targetPos.localPosition, _time);
+
+    }
+}
