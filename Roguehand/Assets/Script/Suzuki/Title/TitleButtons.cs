d0de216442a1395d170698f417f2541013cbe74d
SuzukiYuya <mcd2376083@stu.o-hara.ac.jp>
2026-01-14 14:45:26 +0900

    はじめから、つづきから、おわるが選択できるようになった

--- Roguehand/Assets/Script/Suzuki/Title/TitleButtons.cs
+++ Roguehand/Assets/Script/Suzuki/Title/TitleButtons.cs
@@ -3,29 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TitleStartButton : MonoBehaviour
+public class TitleButtons : MonoBehaviour
 {
     [SerializeField] Button _startButton;
+    [SerializeField] Button _continueButton;
+    [SerializeField] Button _exitButton;
     [SerializeField] GameObject _fadePanel;
     [SerializeField] GameObject _selectDeckPanel;
 
     private void Awake()
     {
         _startButton.onClick.AddListener(OnStart);
+        _continueButton.onClick.AddListener(OnContinue);
+        _exitButton.onClick.AddListener(OnExit);
         _fadePanel.SetActive(false);
         _selectDeckPanel.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void OnStart()
     {
         _fadePanel.SetActive(true);
         _selectDeckPanel.SetActive(true);
+    }
+
+    void OnContinue()
+    {
         //GameSceneManager.LoadScene(GameSceneManager.mainScene);
+    }
+
+    void OnExit()
+    {
+
     }
 }
