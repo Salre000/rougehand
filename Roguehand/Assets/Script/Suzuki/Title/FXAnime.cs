9a1a4ca08752e96d86edc0caa3810c164c69de46
SuzukiYuya <mcd2376083@stu.o-hara.ac.jp>
2026-01-22 11:54:28 +0900

    はじめからを押したときにスムーズにデッキ選択へ、もどることも可能にした

--- Roguehand/Assets/Script/Suzuki/Title/FXAnime.cs
+++ Roguehand/Assets/Script/Suzuki/Title/FXAnime.cs
@@ -1,47 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FXAnime : MonoBehaviour
 {
-    [SerializeField] RectTransform _panel;
-    [SerializeField] Transform _fx;
+    [SerializeField] Button _startButton;
+    [SerializeField] Button _backButton;
+    [SerializeField] GameObject _gameObject;
+    Transform _panel;
+    [SerializeField] RectTransform _fx;
+    [SerializeField] GameObject _fadePanel;
     Vector3 _position = Vector3.zero;
 
-    float _velocity = 0f;
 
-    float _h_fade1 = 10.0f;
+    float _h_fade1 = 1080.0f;
     float _w_fade1 = 2400.0f;
 
-    bool fade1F=false;
+    float _time = 0;
+    float _speed = 0.6f;
+
+    bool _startF = false;
+    bool fade1F = false;
+    bool fade2F = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        _panel = _gameObject.transform;
+        _startButton.onClick.AddListener(StartFlag);
+        _backButton.onClick.AddListener(OnBack);
         _position = _panel.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!_startF) return;
+        FadeMove();
         PanelSavaTrans();
+    }
+
+    void StartFlag()
+    {
+        _startF = true;
+        _fadePanel.SetActive(true);
+    }
+
+    void OnBack()
+    {
+        _gameObject.SetActive(false);
+        _fadePanel.SetActive(false);
+        _fx.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0);
+        _fx.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 0);
+        _time = 0;
+        _startF = false; 
+        fade1F = false;
+    }
+
+    void FadeMove()
+    {
+        _time += _speed * Time.deltaTime;
+        Fade1();
+        Fade2();
     }
 
     void Fade1()
     {
-        _velocity=_panel.rect.height;
-        _velocity = Mathf.Lerp(_panel.rect.height, _h_fade1, 1);
+        if (fade1F) return;
         // 垂直サイズの変更
-        _panel.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _velocity);
+        _fx.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Mathf.Lerp(_fx.rect.height, _h_fade1, _time));
+        // 水平サイズの変更
+        _fx.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Mathf.Lerp(_fx.rect.width, _w_fade1, _time));
 
-        if((_panel.rect.size.y-_h_fade1<0.1f))
-            fade1F = true;
+
+        if (Mathf.Abs(_fx.rect.height - _h_fade1) > 0.1f) return;
+        if (Mathf.Abs(_fx.rect.width - _w_fade1) > 0.1f) return;
+        fade1F = true;
+        _time = 0;
     }
 
     void Fade2()
     {
-        if (!fade1F) return;
+        if (!fade1F || fade2F) return;
 
+        
+        fade2F = true;
+        _time = 0;
     }
 
 
