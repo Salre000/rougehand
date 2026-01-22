37e6c38faef7651e09cba8141f5d36838f1bdc1d
SuzukiYuya <mcd2376083@stu.o-hara.ac.jp>
2026-01-22 12:05:37 +0900

    ショップ画面でもオプションを開けるようにした

--- Roguehand/Assets/Script/Suzuki/Run/OptionOpen.cs
+++ Roguehand/Assets/Script/Suzuki/Run/OptionOpen.cs
@@ -7,16 +7,20 @@ using UnityEngine.UI;
 
 public class OptionOpen : MonoBehaviour
 {
+    [SerializeField] private Button _shopOpButton;
     [SerializeField] private Button _opButton;
     [SerializeField] private Button _opCloseButton;
     [SerializeField] private Button _resButton;
     [SerializeField] private Button _retireButton;
     [SerializeField] private GameObject _opObject;
+    [SerializeField] private Transform _gameTarget;
+    [SerializeField] private Transform _shopTarget;
     // Start is called before the first frame update
     void Start()
     {
         _opButton.onClick.AddListener(Onclick);
         _opCloseButton.onClick.AddListener(Onclick);
+        _shopOpButton.onClick.AddListener(OnShopOpClick);
         _resButton.onClick.AddListener(OnReset);
         _retireButton.onClick.AddListener(OnRetire);
         _opObject.SetActive(false);
@@ -30,10 +34,27 @@ public class OptionOpen : MonoBehaviour
 
     void Onclick()
     {
-        if(_opObject.activeSelf)
+        if (_opObject.activeSelf)
+        {
             _opObject.SetActive(false);
+        }
         else
+        {
+            _opObject.transform.SetPositionAndRotation(_gameTarget.transform.position, _gameTarget.transform.rotation);
             _opObject.SetActive(true);
+        }
+    }
+    void OnShopOpClick()
+    {
+        if (_opObject.activeSelf)
+        {
+            _opObject.SetActive(false);
+        }
+        else
+        {
+            _opObject.transform.SetPositionAndRotation(_shopTarget.transform.position, _shopTarget.transform.rotation);
+            _opObject.SetActive(true);
+        }
     }
 
     private void OnReset()
--- Roguehand/Assets/Script/Suzuki/Title/TitleButtons.cs
+++ Roguehand/Assets/Script/Suzuki/Title/TitleButtons.cs
@@ -5,26 +5,17 @@ using UnityEngine.UI;
 
 public class TitleButtons : MonoBehaviour
 {
-    [SerializeField] Button _startButton;
     [SerializeField] Button _continueButton;
     [SerializeField] Button _exitButton;
-    [SerializeField] GameObject _fadePanel;
-    [SerializeField] GameObject _selectDeckPanel;
+
 
     private void Awake()
     {
-        _startButton.onClick.AddListener(OnStart);
         _continueButton.onClick.AddListener(OnContinue);
         _exitButton.onClick.AddListener(OnExit);
-        _fadePanel.SetActive(false);
-        _selectDeckPanel.SetActive(false);
+
     }
 
-    void OnStart()
-    {
-        _fadePanel.SetActive(true);
-        _selectDeckPanel.SetActive(true);
-    }
 
     void OnContinue()
     {
